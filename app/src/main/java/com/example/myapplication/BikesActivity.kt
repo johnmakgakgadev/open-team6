02b5db7925f3a8bcb0ba9580d327b952db4eccc3
koleladan <isaiahodan@gmail.com>
2022-11-17 18:50:35 +0300

    main activity

--- app/src/main/java/com/example/myapplication/BikesActivity.kt
+++ app/src/main/java/com/example/myapplication/BikesActivity.kt
@@ -2,10 +2,13 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.databinding.ActivityBikesBinding
 
 class BikesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBikesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bikes)
+        binding = ActivityBikesBinding.inflate(layoutInflater)
+                super.onCreate(savedInstanceState)
+        setContentView(binding.root)
     }
 }
