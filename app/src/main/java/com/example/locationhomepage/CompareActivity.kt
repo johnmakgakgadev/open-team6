88024b3e8d1736ca0b1f23b5c942a990155df8c1
Ahmed Hassan <ahmed.code5066@gmail.com>
2022-11-21 19:38:40 +0200

    first commit

---
+++ app/src/main/java/com/example/locationhomepage/CompareActivity.kt
@@ -0,0 +1,11 @@
+package com.example.locationhomepage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CompareActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compare)
+    }
+}
