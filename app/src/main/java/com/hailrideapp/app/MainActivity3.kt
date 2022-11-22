1bc10e5f84494b674e92d249c09eaa93bff488de
DonMwenesi <117765753+DonMwenesi@users.noreply.github.com>
2022-11-22 17:41:36 +0300

    User Interface log in and registration

--- app/src/main/java/com/hailrideapp/app/MainActivity3.kt
+++ app/src/main/java/com/hailrideapp/app/MainActivity3.kt
@@ -18,10 +18,30 @@ class MainActivity3 : AppCompatActivity() {
         setContentView(R.layout.activity_main3);
 
 
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
 
 
 
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
 
+        //admin and admin
+
+        val loginbtn = null
+        loginbtn.SetOnClickListener(new View.OnClickListener() {
+            val public
+            override@
+            public void onClick(View v) {
+                if (!(!username.getText().string().equals("admin") || !password.getText().string().equals("admin"))){
+                    //correct
+                    Toast.makeText(context: MainActivity.this, text: "LOGIN SUCCESSFUL", Toast.LENGHT_SHORT).show();
+                }else
+                //incorrect
+                    Toast.makeText(context: MainActivity.this, text: "LOGIN FAILED !!!", Toast.LENGHT_SHORT).show();
+
+            }
+            }
+        })
 
 
     }
